35cd3ce0fc4b52595c55566a62944253acf6b8e6
Andrei <andrei.dimitrascu.94@gmail.com>
2020-04-26 18:54:51 +0300

    deploy

---
+++ build/precache-manifest.cc44a4092268e0ac1b60fc5afd228f0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7f7be8e201475422f4fc1adc0238ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d24d28a76a0d8c9a8855",
+    "url": "/static/css/main.13d784dd.chunk.css"
+  },
+  {
+    "revision": "50b2e459d25667e2841c",
+    "url": "/static/js/2.37c0a557.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37c0a557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d24d28a76a0d8c9a8855",
+    "url": "/static/js/main.788f6b71.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
